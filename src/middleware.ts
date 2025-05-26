1bb349290c9796c87e3bed72f9772b52137862ee
doomling <bethlejem@gmail.com>
2025-05-26 14:56:20 -0300

    fix/cookie not generating

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,25 +4,22 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // Solo afecta al home
   if (pathname === "/") {
     const version = request.cookies.get("version");
 
-    // Si no hay cookie, asignamos una versión
     if (!version) {
       const random = Math.random();
-      const response = NextResponse.next();
-
-      // Cambiá este valor para aumentar el rollout (ej: 0.05 = 5%)
       const rolloutPercentage = 0.1;
 
       if (random < rolloutPercentage) {
+        const response = NextResponse.redirect(new URL("/new", request.url));
         response.cookies.set("version", "new", {
           path: "/",
           maxAge: 60 * 60 * 24 * 30,
         });
-        return NextResponse.redirect(new URL("/new", request.url));
+        return response;
       } else {
+        const response = NextResponse.redirect(new URL("/", request.url)); // redirige aunque sea al mismo lugar
         response.cookies.set("version", "stable", {
           path: "/",
           maxAge: 60 * 60 * 24 * 30,
@@ -31,7 +28,6 @@ export function middleware(request: NextRequest) {
       }
     }
 
-    // Si ya hay cookie, respetamos la asignación
     if (version.value === "new") {
       return NextResponse.redirect(new URL("/new", request.url));
     }
